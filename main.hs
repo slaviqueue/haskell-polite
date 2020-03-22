49e46cf096d092ce218719dd04e77b1f0c4273d0
Slava <electricsemyon@gmail.com>
2020-03-22 16:43:08 +0200

    Create main.hs

---
+++ main.hs
@@ -0,0 +1,12 @@
+calculate :: String -> [Int]
+calculate = foldl calcop [] . words
+
+calcop :: [Int] ->  String -> [Int]
+calcop (x:y:ys) "+" = (x + y):ys
+calcop (x:y:ys) "-" = (x - y):ys
+calcop (x:y:ys) "/" = (x `div` y):ys
+calcop (x:y:ys) "*" = (x * y):ys
+calcop xs c = (read c):xs
+
+main =
+  print $ calculate "2 3 + 3 *"
